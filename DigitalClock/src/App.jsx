3c7f77665021e0a476f85311cda80ef1bafba80a
Cinerarius <koustavrakshit10b@gmail.com>
2023-04-25 00:43:38 +0530

    am||pm

--- DigitalClock/src/App.jsx
+++ DigitalClock/src/App.jsx
@@ -12,16 +12,19 @@ function Clock() {
   let displayHour = 0;
   let displayMin = 0;
   let displaySec = 0;
+  let session = 0;
   let hour = time.getHours();
   if (hour < 9) displayHour = `0${hour}`;
   else displayHour = hour;
+  if (hour >= 12) session = "PM";
+  else session = "AM";
   let min = time.getMinutes();
   if (min < 9) displayMin = `0${min}`;
   else displayMin = min;
   let sec = time.getSeconds();
   if (sec < 9) displaySec = `0${sec}`;
   else displaySec = sec;
-  let showTime = `${displayHour}:${displayMin}:${displaySec}`;
+  let showTime = `${displayHour}:${displayMin}:${displaySec} ${session}`;
   return <h1>{showTime}</h1>;
 }
 
